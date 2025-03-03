7a01ecdf0653e2899426c683b8f4f29f324f626e
ValentinBD10 <valentinbd10@gmail.com>
2025-03-03 22:23:03 +0100

    milieu

--- TP4/src/app/page.tsx
+++ TP4/src/app/page.tsx
@@ -1,11 +1,18 @@
+import { getPosts } from "@/actions/post.action";
 import { ModeToggle } from "@/components/modetoggle";
 import { Button } from "@/components/ui/button";
+import { currentUser } from "@clerk/nextjs/server";
+import { getDbUserId } from "@/actions/user.action";
 import {
   SignInButton,
   SignedIn,
   SignedOut,
   UserButton,
 } from '@clerk/nextjs';
+import CreatePost from "@/components/createpost";
+import WhoToFollow from "@/components/whotofollow";
+
+
 
 export default async function Home() {
   const user = await currentUser();
